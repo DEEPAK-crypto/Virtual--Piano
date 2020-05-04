c322defee10a5d3e4554fd70543fcdaf0be08b6e
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-04 17:59:38 +0530

    added keyup functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,90 +13,137 @@ var x = document.getElementById("f4");
 var c = document.getElementById("g3");
 var v = document.getElementById("g4");
 
+
 document.addEventListener("keydown", function(key) {
-    if (key.which == 65) {
+    if (key.which == 72) {
+        document.querySelector(".a3").classList.toggle("border");
         if (a.paused)
             a.play();
         else {
             a.currentTime = 0;
         }
-    } else if (key.which == 83) {
+    } else if (key.which == 67) {
+        document.querySelector(".a4").classList.toggle("border");
         if (s.paused)
             s.play();
         else {
             s.currentTime = 0;
         }
-    } else if (key.which == 68) {
+    } else if (key.which == 74) {
+        document.querySelector(".b3").classList.toggle("border");
         if (d.paused)
             d.play();
         else {
             d.currentTime = 0;
         }
-    } else if (key.which == 70) {
+    } else if (key.which == 86) {
+        document.querySelector(".b4").classList.toggle("border");
         if (f.paused)
             f.play();
         else {
             f.currentTime = 0;
         }
-    } else if (key.which == 71) {
+    } else if (key.which == 65) {
+        document.querySelector(".c3").classList.toggle("border");
         if (g.paused)
             g.play();
         else {
             g.currentTime = 0;
         }
-    } else if (key.which == 72) {
+    } else if (key.which == 75) {
+        document.querySelector(".c4").classList.toggle("border");
         if (h.paused)
             h.play();
         else {
             h.currentTime = 0;
         }
-    } else if (key.which == 74) {
+    } else if (key.which == 83) {
+        document.querySelector(".d3").classList.toggle("border");
         if (j.paused)
             j.play();
         else {
             j.currentTime = 0;
         }
-    } else if (key.which == 75) {
+    } else if (key.which == 76) {
+        document.querySelector(".d4").classList.toggle("border");
         if (k.paused)
             k.play();
         else {
             k.currentTime = 0;
         }
-    } else if (key.which == 76) {
+    } else if (key.which == 68) {
+        document.querySelector(".e3").classList.toggle("border");
         if (l.paused)
             l.play();
         else {
             l.currentTime = 0;
         }
     } else if (key.which == 186) {
+        document.querySelector(".e4").classList.toggle("border");
         if (g4.paused)
             g4.play();
         else {
             g4.currentTime = 0;
         }
-    } else if (key.which == 90) {
+    } else if (key.which == 70) {
+        document.querySelector(".f3").classList.toggle("border");
         if (z.paused)
             z.play();
         else {
             z.currentTime = 0;
         }
-    } else if (key.which == 88) {
+    } else if (key.which == 90) {
+        document.querySelector(".f4").classList.toggle("border");
         if (x.paused)
             x.play();
         else {
             x.currentTime = 0;
         }
-    } else if (key.which == 67) {
+    } else if (key.which == 71) {
+        document.querySelector(".g3").classList.toggle("border");
         if (c.paused)
             c.play();
         else {
             c.currentTime = 0;
         }
-    } else if (key.which == 86) {
+    } else if (key.which == 88) {
+        document.querySelector(".g4").classList.toggle("border");
         if (v.paused)
             v.play();
         else {
             v.currentTime = 0;
         }
     }
+
+
 });
+document.addEventListener("keyup", function(key) {
+    if (key.which == 72)
+        document.querySelector(".a3").classList.toggle("border");
+    else if (key.which == 88)
+        document.querySelector(".g4").classList.toggle("border");
+    else if (key.which == 71)
+        document.querySelector(".g3").classList.toggle("border");
+    else if (key.which == 90)
+        document.querySelector(".f4").classList.toggle("border");
+    else if (key.which == 70)
+        document.querySelector(".f3").classList.toggle("border");
+    else if (key.which == 186)
+        document.querySelector(".e4").classList.toggle("border");
+    else if (key.which == 68)
+        document.querySelector(".e3").classList.toggle("border");
+    else if (key.which == 76)
+        document.querySelector(".d4").classList.toggle("border");
+    else if (key.which == 83)
+        document.querySelector(".d3").classList.toggle("border");
+    else if (key.which == 75)
+        document.querySelector(".c4").classList.toggle("border");
+    else if (key.which == 86)
+        document.querySelector(".b4").classList.toggle("border");
+    else if (key.which == 67)
+        document.querySelector(".a4").classList.toggle("border");
+    else if (key.which == 74)
+        document.querySelector(".b3").classList.toggle("border");
+    else if (key.which == 65)
+        document.querySelector(".c3").classList.toggle("border");
+})
